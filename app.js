fdf547f31ba62ddab59c8774a9ab5ce2710c57a7
Sergei Mukhin <sergei_mukhin@outlook.com>
2025-01-13 14:20:41 +0300

    feat: init git repo for zabbix-client plugin for IntraScada

---
+++ app.js
@@ -0,0 +1,29 @@
+const util = require("util");
+const axios = require("axios");
+
+module.exports = async function (plugin) {
+  let toSend = [];
+  let groupChannels;
+
+  sendNext();
+  main(plugin.channels.data);
+
+  function main(channels) {
+    groupChannels = groupByUniq(channels, "sessionname");
+    monitor();
+  }
+
+  function sendNext() {
+    if (toSend.length > 0) {
+      plugin.sendData(toSend);
+      toSend = [];
+    }
+    setTimeout(sendNext, 2500);
+  }
+
+  function monitor() {
+    setTimeout(() => {
+      monitor();
+    }, 5000);
+  }
+};
---
+++ index.js
@@ -0,0 +1,30 @@
+const util = require("util");
+const app = require("./app");
+
+(async () => {
+  let plugin;
+  try {
+    const opt = getOptFromArgs();
+    const pluginapi = opt && opt.pluginapi ? opt.pluginapi : "ih-plugin-api";
+    plugin = require(pluginapi + "/index.js")();
+
+    plugin.log("Plugin zabbix client has started.", 0);
+
+    plugin.params.data = await plugin.params.get();
+    plugin.channels.data = await plugin.channels.get();
+
+    app(plugin);
+  } catch (err) {
+    plugin.exit(8, `Error: ${util.inspect(err)}`);
+  }
+})();
+
+function getOptFromArgs() {
+  let opt;
+  try {
+    opt = JSON.parse(process.argv[2]);
+  } catch (e) {
+    opt = {};
+  }
+  return opt;
+}
