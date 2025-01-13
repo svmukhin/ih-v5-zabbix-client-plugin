ba86b698e81ad4cf2591fa5efca65ec24cb9ae5e
Sergei Mukhin <sergei_mukhin@outlook.com>
2025-01-14 02:41:59 +0300

    feat: add fetch items last values from zabbix server
    Add interval property to plugin common form
    
    When plugin start:
    - Save uri, username, pass and interval to local vars
    - Request auth token from zabbix server and save it in local var
    - Request and save hostIds from zabbix server by uniq set of hostnames
    - Save uniq itemkeys to local array
    - Launch monitor function
    
    Monitor function:
    - Request items with extend output from zabbix server (filter by
      hostids and itemkeys
    - For each channel try to find item in result by hostname and itemkey
    - If key found, push to send array last item value, else push to send
      array chstatus: 1
    - Restart monitor function after timeout (use interval value for
      timeout)
    
    SendNext function:
    - Restart every interval/2 sec
    - If array to send not empty, send data to server

--- app.js
+++ app.js
@@ -2,14 +2,37 @@ const util = require("util");
 const axios = require("axios");
 
 module.exports = async function (plugin) {
+  let auth;
+  let apiUrl;
+  let apiUsername;
+  let apiPassword;
   let toSend = [];
-  let groupChannels;
+  let requestId;
+  let hostIds = [];
+  let itemKeys = [];
+  let interval = 15000;
 
   sendNext();
-  main(plugin.channels.data);
+  main(plugin);
 
-  function main(channels) {
-    monitor();
+  async function main(plugin) {
+    apiUrl = plugin.params.data.apiuri;
+    apiUsername = plugin.params.data.username;
+    apiPassword = plugin.params.data.password;
+    if (plugin.params.data.interval > 0) {
+      interval = plugin.params.data.interval * 1000;
+    }
+
+    auth = authenticate(apiUsername, apiPassword);
+
+    hostIds = await getHostIds(plugin.channels.data);
+
+    let keys = plugin.channels.data.map((channel) => {
+      channel.itemkey;
+    });
+    itemKeys = [...new Set(keys)];
+
+    monitor(plugin.channels.data);
   }
 
   function sendNext() {
@@ -17,12 +40,95 @@ module.exports = async function (plugin) {
       plugin.sendData(toSend);
       toSend = [];
     }
-    setTimeout(sendNext, 2500);
+    setTimeout(sendNext, interval / 2);
   }
 
-  function monitor() {
+  async function monitor(channels) {
+    try {
+      const response = await axios.post(apiUrl, {
+        jsonrpc: "2.0",
+        method: "item.get",
+        params: {
+          output: ["extend"],
+          hostids: hostIds,
+          search: { key_: uniqueKeys },
+        },
+        auth: auth,
+        id: requestId,
+      });
+
+      requestId++;
+      let items = response.data.result;
+      channels.forEach((channel) => {
+        let item = items.find((it) => {
+          return channel.itemkey == it.key_ && channel.hostname == it.host;
+        });
+
+        if (item) {
+          toSend.push({
+            id: channel.id,
+            value: item.lastvalue,
+            chstatus: 0,
+            ts: new Date().getTime(),
+          });
+        } else {
+          toSend.push({
+            id: channel.id,
+            chstatus: 1,
+          });
+        }
+      });
+    } catch (error) {
+      plugin.log("Error in monitor: " + util.inspect(error));
+    }
+
     setTimeout(() => {
-      monitor();
-    }, 5000);
+      monitor(channels);
+    }, interval);
+  }
+
+  async function authenticate(username, password) {
+    try {
+      requestId = 1;
+      const response = await axios.post(apiUrl, {
+        jsonrpc: "2.0",
+        method: "user.login",
+        params: { user: username, password: password },
+        id: requestId,
+        auth: null,
+      });
+
+      requestId++;
+      return response.data.result;
+    } catch (error) {
+      plugin.log("Error in authenticate: " + util.inspect(error));
+    }
+  }
+
+  async function getHostIds(channels) {
+    try {
+      let hosts = channels.map((channel) => {
+        channel.hostname;
+      });
+      let uniqueHosts = [...new Set(hosts)];
+
+      const response = await axios.post(apiUrl, {
+        jsonrpc: "2.0",
+        method: "host.get",
+        params: {
+          output: ["hostid"],
+          filter: { host: uniqueHosts },
+        },
+        auth: auth,
+        id: requestId,
+      });
+
+      requestId++;
+      return response.data.result.map((host) => {
+        host.hostid;
+      });
+    } catch (error) {
+      plugin.log("Error in getHostIds: " + util.inspect(error));
+    }
   }
 };
